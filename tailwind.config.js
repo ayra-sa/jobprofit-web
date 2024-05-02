0b372c3be35cc560ce195947c78d2d0046d38925
ayra-sa <rizkyaryas11@gmail.com>
2024-05-02 15:35:56 +0700

    fix input

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,9 +11,6 @@ module.exports = {
     },
     extend: {
       colors: {
-        border: "hsl(var(--border))",
-        input: "hsl(var(--input))",
-        ring: "hsl(var(--ring))",
         pending: "hsl(var(--pending))",
         success: "hsl(var(--success))",
         background: "hsl(var(--background))",
@@ -25,18 +22,6 @@ module.exports = {
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
-        },
-        destructive: {
-          DEFAULT: "hsl(var(--destructive))",
-          foreground: "hsl(var(--destructive-foreground))",
-        },
-        muted: {
-          DEFAULT: "hsl(var(--muted))",
-          foreground: "hsl(var(--muted-foreground))",
-        },
-        accent: {
-          DEFAULT: "hsl(var(--accent))",
-          foreground: "hsl(var(--accent-foreground))",
         },
         card: {
           DEFAULT: "hsl(var(--card))",
