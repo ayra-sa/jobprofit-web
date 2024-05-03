613c9a9f19b4ee0387f6eb092fb4f181fa067e5e
ayra-sa <rizkyaryas11@gmail.com>
2024-05-03 07:19:37 +0700

    add button hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,10 +18,14 @@ module.exports = {
         primary: {
           DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
+          hover: "hsl(var(--primary-hover))",
+          active: "hsl(var(--primary-active))",
         },
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
+          hover: "hsl(var(--secondary-hover))",
+          active: "hsl(var(--secondary-active))",
         },
         card: {
           DEFAULT: "hsl(var(--card))",
